2c466ec72c711e050b5c1fc525205cc97d473092
Ayush Garg <ayush82099@gmail.com>
2026-04-24 22:02:35 +0530

    Fix Express v5 wildcard route crash on startup

--- backend/index.js
+++ backend/index.js
@@ -144,7 +144,7 @@ Your responses should be:
 // =============================================
 // Serve React App (Catch-all)
 // =============================================
-app.get('*', (req, res) => {
+app.get('/{*splat}', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend/dist/index.html'));
 });
 
